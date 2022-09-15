c0a38c2e954311bd03243119c52147483b227a87
Tyrone Meijn <tyrone_meijn@outlook.com>
2022-09-15 20:33:59 +0200

    enable fontconfig

--- nixpkgs/home-manager/ubuntu.nix
+++ nixpkgs/home-manager/ubuntu.nix
@@ -15,6 +15,8 @@
 
   home.stateVersion = "22.05";
 
+  fonts.fontconfig.enable = true;
+
   # http://czyzykowski.com/posts/gnupg-nix-osx.html
   # adds file to `~/.nix-profile/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac`
   home.packages = with pkgs; [
