f2f9121d182a8a1950aac82f9478d3a684d58f03
Tyrone Meijn <tyrone_meijn@outlook.com>
2024-01-21 13:46:58 +0000

    feat: add scripts for ghorg and setting up ssh

--- chezmoi/.chezmoiscripts/run_once_after_0_bootstrap.sh
+++ chezmoi/.chezmoiscripts/run_once_after_0_bootstrap.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env -S bash -i
 
-# Nothing to do here
+# Change Fn key to be released by default
+if [ -d /sys/module/hid_apple ]; then
+  echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+  echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_fn_leftctrl
+fi
