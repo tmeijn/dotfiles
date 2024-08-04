3d220e49a7b31b0d71bc2d6b3b75c0442bd63408
Tyrone Meijn <tyrone_meijn@outlook.com>
2024-08-04 16:36:40 +0200

    added paths back in

--- chezmoi/private_dot_local/private_share/private_gnome-shell/extensions/rounded-window-corners@fxgn/effect/linear_filter_effect.js
+++ chezmoi/private_dot_local/private_share/private_gnome-shell/extensions/rounded-window-corners@fxgn/effect/linear_filter_effect.js
@@ -6,7 +6,7 @@ export const LinearFilterEffect = GObject.registerClass({}, class extends Shell.
         this.add_glsl_snippet(Shell.SnippetHook.FRAGMENT, '', '', false);
     }
     vfunc_paint_target(node, ctx) {
-        this.get_pipeline()?.set_layer_filters(0, Cogl.PipelineFilter.LINEAR_MIPMAP_LINEAR, Cogl.PipelineFilter.NEAREST);
+        this.get_pipeline()?.set_layer_filters(0, Cogl.PipelineFilter.LINEAR_MIPMAP_LINEAR, Cogl.PipelineFilter.LINEAR);
         super.vfunc_paint_target(node, ctx);
     }
 });
