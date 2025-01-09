9669d23dfceb3bbb2fb76fd8bd873b2b8ad9cbb0
Tyrone Meijn <tyrone_meijn@outlook.com>
2025-01-09 12:43:18 +0100

    install cmake dep

--- chezmoi/.chezmoiscripts/run_once_after_30_install-tools-with-mise.sh
+++ chezmoi/.chezmoiscripts/run_once_after_30_install-tools-with-mise.sh
@@ -8,8 +8,6 @@ echo ""
 echo "==============================================================="
 echo ""
 
-echo "Installing Git as Mise needs that as dependency"
-sudo apt install git -y
 
 wget -qO- https://mise.run/ | sh
 
@@ -18,5 +16,8 @@ sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+echo "Installing Cmake for building Onefetch..."
+sudo apt-get install -y cmake"
 
 mise install --yes
