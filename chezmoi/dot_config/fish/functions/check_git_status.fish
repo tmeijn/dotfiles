e991ac1aacc0d73bf8fe8f42349fc60431418fdf
Tyrone Meijn <tyrone_meijn@outlook.com>
2025-01-29 11:16:24 +0000

    feat(fish): add wrapper around mkdir and cd

--- chezmoi/dot_config/fish/functions/check_git_status.fish
+++ chezmoi/dot_config/fish/functions/check_git_status.fish
@@ -1,13 +1,13 @@
 # Function to check if a git repository has pending changes
 function check_git_status
-  find . -type d -name ".git" ! -path "*/.terraform/modules*" -exec dirname {} \; | while read -l dir
-      if test -d "$dir/.git"
-          pushd "$dir" > /dev/null; or return
-          if git status --porcelain | grep -q .
-              echo "=== Pending changes in $dir ==="
-              git status -s
-          end
-          popd > /dev/null; or return
-      end
-  end
+    find . -type d -name ".git" ! -path "*/.terraform/modules*" -exec dirname {} \; | while read -l dir
+        if test -d "$dir/.git"
+            pushd "$dir" >/dev/null; or return
+            if git status --porcelain | grep -q .
+                echo "=== Pending changes in $dir ==="
+                git status -s
+            end
+            popd >/dev/null; or return
+        end
+    end
 end
